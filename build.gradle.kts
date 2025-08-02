d19a5589cff5d97311459ee0374e9ed9e59d6655
Lunqia <31104268+Lunqia@users.noreply.github.com>
2025-08-02 19:32:14 +0200

    feat: Configure JPA settings and initialize tasks table in database

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.projectlombok:lombok")
     implementation("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.flywaydb:flyway-core")
     annotationProcessor("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
---
+++ src/main/resources/db/migration/V1__init_tasks_table.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS tasks;
+
+CREATE TABLE tasks (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    description VARCHAR(255) NOT NULL,
+    completed BIT NOT NULL DEFAULT 0
+);
